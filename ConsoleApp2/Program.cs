c83a9288191d1ac3b1f8a7299b9e98042ac1bf14
Moamen28 <moamne2819@outlook.com>
2022-04-19 00:38:46 +0200

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connetionString;
+                SqlConnection cnn;
+                connetionString = @"Data Source=DESKTOP-D26N3LP;Initial Catalog=moamen;Integrated Security=True";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                SqlCommand command = cnn.CreateCommand();
+                if (cnn.State == ConnectionState.Open)
+                {
+                    command.CommandText = "INSERT INTO tt VALUES ('moamenjnddac')";
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Inserting Data Successfully");
+                                                }
+            }
+            catch
+            {
+                Console.WriteLine("Exp");
+            }
+          
+        }
+    }
+}
